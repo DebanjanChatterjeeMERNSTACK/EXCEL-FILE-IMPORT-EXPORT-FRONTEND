bb3e6f154a6c560d728afb0a2b4653dc1e581173
DebanjanChatterjeeMERNSTACK <debanjanthechatterjee@gmail.com>
2023-08-23 09:55:18 -0700

    CHANGES

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,79 @@
 import React, { useEffect, useState } from 'react'
-
+import FileSaver from 'file-saver'
 
 function App() {
   const [file, setFile] = useState("")
-  const [user,setuser]=useState([])
+  const [user, setuser] = useState([])
 
 
 
   useEffect(() => {
-    fetch("http://localhost:9000/getexcel")
-    .then(res=>res.json())
-    .then(json=>setuser(json.user))
+    fetch("http://localhost:8000/getexcel")
+      .then(res => res.json())
+      .then(json => setuser(json.user))
   }, [user])
 
+
+  const handleexport = () => {
+
+    fetch("http://localhost:8000/exportexcel")
+      .then(res => res.blob())
+       .then(blob => FileSaver(blob,"salarysheet"))
+  }
 
 
 
   const handleupload = () => {
-   const fromdata= new FormData()
-   fromdata.append("file", file)
-  
+    const fromdata = new FormData()
+    fromdata.append("file", file)
 
-   fetch("http://localhost:9000/excel",{
-    method:"POST",
-    body:fromdata
-   }).then((res)=>res.json())
-   .then((json)=>alert(json.mess))
+
+    fetch("http://localhost:8000/excel", {
+      method: "POST",
+      body: fromdata
+    }).then((res) => res.json())
+      .then((json) => alert(json.mess))
 
   }
 
   return (
     <>
-      <div  className='csv'>
+      <div className='csv'>
         <p>UPLOAD ONLY EXCEL FILE</p>
-        <div  className='csvflex'>
-          <input type='file' className='' required onChange={(e) => setFile(e.target.files[0])} />
+        <div className='csvflex'>
+          <input type='file' className='' required accept='.xlsx' onChange={(e) => setFile(e.target.files[0])} />
           <button type="button" className="btn btn-primary" onClick={handleupload}>upload</button>
+
         </div>
+        <button type="button" className="btn btn-primary" onClick={handleexport}>Export</button>
 
         <div className='csvsc'>
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">Name</th>
-              <th scope="col">Address</th>
-              <th scope="col">PhoneNo</th>
-              <th scope="col">Salary</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              user && user.map((e,ind)=>{
-                return(
-                  <tr  key={ind}>
-                  <td>{e.Name}</td>
-                  <td>{e.Address}</td>
-                  <td>{e.PhoneNumber}</td>
-                  <td>{e.Salary}</td>
-                </tr>
-                )
-              })
-            }
-           
-          </tbody>
-        </table>
-      </div>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Address</th>
+                <th scope="col">PhoneNo</th>
+                <th scope="col">Salary</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                user && user.map((e, ind) => {
+                  return (
+                    <tr key={ind}>
+                      <td>{e.Name}</td>
+                      <td>{e.Address}</td>
+                      <td>{e.PhoneNumber}</td>
+                      <td>{e.Salary}</td>
+                    </tr>
+                  )
+                })
+              }
+
+            </tbody>
+          </table>
+        </div>
       </div>
 
     </>
